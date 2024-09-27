ff24672d384fd57b0cb425f74ca0be0a482bcaeb
KevinVel16 <90013072+kevinevelasco@users.noreply.github.com>
2024-09-26 19:05:17 -0500

    first commit

---
+++ src/main/java/co/edu/javeriana/msc/turismo/service_query_microservice/ServiceQueryMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package co.edu.javeriana.msc.turismo.service_query_microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceQueryMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceQueryMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/javeriana/msc/turismo/service_query_microservice/ServiceQueryMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.javeriana.msc.turismo.service_query_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceQueryMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
