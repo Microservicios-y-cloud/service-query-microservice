d7db899bec7984006b051eeae0e7f74f95493a0a
KevinVel16 <90013072+kevinevelasco@users.noreply.github.com>
2024-09-26 23:18:21 -0500

    configuración para recepción de mensaje

---
+++ src/main/java/co/edu/javeriana/msc/turismo/service_query_microservice/enums/ServiceType.java
@@ -0,0 +1,7 @@
+package co.edu.javeriana.msc.turismo.service_publication_microservice.enums;
+
+public enum ServiceType {
+    ACCOMMODATION,
+    FOOD,
+    TRANSPORTATION,
+}
---
+++ src/main/java/co/edu/javeriana/msc/turismo/service_query_microservice/queue/MessageQueueConsumer.java
@@ -0,0 +1,2 @@
+package co.edu.javeriana.msc.turismo.service_query_microservice.queue;public class MessageQueueConsumer {
+}
