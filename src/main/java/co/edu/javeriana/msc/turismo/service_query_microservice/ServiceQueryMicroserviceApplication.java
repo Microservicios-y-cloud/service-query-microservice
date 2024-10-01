4aae973d56ce271488b72e96b7a224137c0485a8
KevinVel16 <90013072+kevinevelasco@users.noreply.github.com>
2024-09-30 19:56:49 -0500

    se corrige error a la hora de iniciar

--- src/main/java/co/edu/javeriana/msc/turismo/service_query_microservice/ServiceQueryMicroserviceApplication.java
+++ src/main/java/co/edu/javeriana/msc/turismo/service_query_microservice/ServiceQueryMicroserviceApplication.java
@@ -2,10 +2,11 @@ package co.edu.javeriana.msc.turismo.service_query_microservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication
 @EnableDiscoveryClient
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class ServiceQueryMicroserviceApplication {
 
 	public static void main(String[] args) {
