e47aab54220a8e60e20809318bbea1d03afbb9eb
KevinVel16 <90013072+kevinevelasco@users.noreply.github.com>
2024-10-31 22:22:34 -0500

    corrección y verificación de excepciones y envío de códigos

--- src/main/java/co/edu/javeriana/msc/turismo/service_query_microservice/advice/GlobalControllerExceptionHandler.java
+++ src/main/java/co/edu/javeriana/msc/turismo/service_query_microservice/advice/GlobalControllerExceptionHandler.java
@@ -1,5 +1,6 @@
 package co.edu.javeriana.msc.turismo.service_query_microservice.advice;
 
+import jakarta.ws.rs.ProcessingException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -22,6 +23,13 @@ public class GlobalControllerExceptionHandler {
     @ExceptionHandler(IllegalArgumentException.class)
     public String handleBadRequest(IllegalArgumentException e) {
         log.error("Bad request: {}", e.getMessage());
+        return e.getMessage();
+    }
+
+    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+    @ExceptionHandler(ProcessingException.class)
+    public String handleProcessingException(ProcessingException e) {
+        log.error("Error processing message: {}", e.getMessage());
         return e.getMessage();
     }
 
--- src/main/java/co/edu/javeriana/msc/turismo/service_query_microservice/queue/MessageQueueConsumer.java
+++ src/main/java/co/edu/javeriana/msc/turismo/service_query_microservice/queue/MessageQueueConsumer.java
@@ -7,19 +7,16 @@ import co.edu.javeriana.msc.turismo.service_query_microservice.dto.TransportType
 import co.edu.javeriana.msc.turismo.service_query_microservice.dto.queue.LocationDTO;
 import co.edu.javeriana.msc.turismo.service_query_microservice.dto.queue.ServiceTypeDTO;
 import co.edu.javeriana.msc.turismo.service_query_microservice.dto.queue.SuperServiceDTO;
-import co.edu.javeriana.msc.turismo.service_query_microservice.enums.CRUDEventType;
 import co.edu.javeriana.msc.turismo.service_query_microservice.repository.*;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.ws.rs.ProcessingException;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.Message;
-
 import java.util.function.Consumer;
 
-import co.edu.javeriana.msc.turismo.service_query_microservice.enums.ServiceType;
-
-import static co.edu.javeriana.msc.turismo.service_query_microservice.enums.ServiceType.*;
 
 @Slf4j
 @Configuration
@@ -34,30 +31,37 @@ public class MessageQueueConsumer {
     @Bean
     Consumer<Message<SuperServiceDTO>> receiveMessage() {
         return message -> {
-            log.info("Received message: {}", message);
-            log.info("Payload: {}", message.getPayload());
-            //hacemos un switch case en caso de que sea CREATE, UPDATE, O DELETE
-            switch (message.getPayload().getEventType()) {
-                case CREATE:
-                    repository.save(message.getPayload().getSuperService());
-                    log.info("Service saved: {}", message.getPayload().getSuperService());
-                    break;
-                case UPDATE:
-                    var serviceToUpdate = repository.findById(message.getPayload().getSuperService().id()).orElse(null);
-                    if(serviceToUpdate != null) {
+            try{
+                log.info("Received message: {}", message);
+                log.info("Payload: {}", message.getPayload());
+                //hacemos un switch case en caso de que sea CREATE, UPDATE, O DELETE
+                switch (message.getPayload().getEventType()) {
+                    case CREATE -> {
                         repository.save(message.getPayload().getSuperService());
-                        log.info("Service updated: {}", message.getPayload().getSuperService().id());
-                    } else {
-                        log.error("Service not found: {}", message.getPayload().getSuperService().id());
+                        log.info("Service saved: {}", message.getPayload().getSuperService());
                     }
-                    break;
-                case DELETE:
-                    repository.deleteById(message.getPayload().getSuperService().id());
-                    log.info("Service deleted: {}", message.getPayload().getSuperService().id());
-                    break;
-                default:
-                    log.error("Invalid action: {}", message.getPayload().getEventType());
-                    break;
+                    case UPDATE -> {
+                        var serviceToUpdate = repository.findById(message.getPayload().getSuperService().id()).orElse(null);
+                        if (serviceToUpdate != null) {
+                            repository.save(message.getPayload().getSuperService());
+                            log.info("Service updated: {}", message.getPayload().getSuperService().id());
+                        } else {
+                            log.error("Service not found: {}", message.getPayload().getSuperService().id());
+                            throw new EntityNotFoundException("Service not found: " + message.getPayload().getSuperService().id());
+                        }
+                    }
+                    case DELETE -> {
+                        repository.deleteById(message.getPayload().getSuperService().id());
+                        log.info("Service deleted: {}", message.getPayload().getSuperService().id());
+                    }
+                    default -> {
+                        log.error("Invalid action: {}", message.getPayload().getEventType());
+                        throw new IllegalArgumentException("Invalid action: " + message.getPayload().getEventType());
+                    }
+                }
+            } catch (Exception e) {
+                log.error("Error processing message: {}", e.getMessage());
+                throw new ProcessingException("Error processing SuperService message. Message received but could not be processed.", e);
             }
         };
     }
@@ -65,38 +69,42 @@ public class MessageQueueConsumer {
     @Bean
     Consumer<Message<ServiceTypeDTO>> receiveServiceType() {
         return message -> {
-            log.info("Received ServiceType: {}", message);
-            log.info("Payload: {}", message.getPayload());
-
-            switch (message.getPayload().getEventType()) {
-                case CREATE:
-                    switch (message.getPayload().getType().type()) {
-                        case ACCOMMODATION -> {
-                            accommodationTypeRepository.save(new AccommodationTypeResponse(
-                                    message.getPayload().getType().id(),
-                                    message.getPayload().getType().name()
-                            ));
-                            log.info("Accommodation saved: {}", message.getPayload().getType());
+            try{
+                log.info("Received ServiceType: {}", message);
+                log.info("Payload: {}", message.getPayload());
+                switch (message.getPayload().getEventType()) {
+                    case CREATE:
+                        switch (message.getPayload().getType().type()) {
+                            case ACCOMMODATION -> {
+                                accommodationTypeRepository.save(new AccommodationTypeResponse(
+                                        message.getPayload().getType().id(),
+                                        message.getPayload().getType().name()
+                                ));
+                                log.info("Accommodation saved: {}", message.getPayload().getType());
+                            }
+                            case FOOD -> {
+                                foodTypeRepository.save(
+                                        new FoodTypeResponse(
+                                                message.getPayload().getType().id(),
+                                                message.getPayload().getType().name()
+                                        )
+                                );
+                                log.info("Food saved: {}", message.getPayload().getType());
+                            }
+                            case TRANSPORTATION -> {
+                                transportTypeRepository.save(new TransportTypeResponse(
+                                        message.getPayload().getType().id(),
+                                        message.getPayload().getType().name()
+                                ));
+                                log.info("Transport saved: {}", message.getPayload().getType());
+                            }
                         }
-                        case FOOD -> {
-                            foodTypeRepository.save(
-                                    new FoodTypeResponse(
-                                            message.getPayload().getType().id(),
-                                            message.getPayload().getType().name()
-                                    )
-                            );
-                            log.info("Food saved: {}", message.getPayload().getType());
-                        }
-                        case TRANSPORTATION -> {
-                            transportTypeRepository.save(new TransportTypeResponse(
-                                    message.getPayload().getType().id(),
-                                    message.getPayload().getType().name()
-                            ));
-                            log.info("Transport saved: {}", message.getPayload().getType());
-                        }
-                    }
-                case UPDATE:
-                    System.out.println("UPDATE");
+                    case UPDATE:
+                        System.out.println("UPDATE");
+                }
+            } catch (Exception e) {
+                log.error("Error processing message: {}", e.getMessage());
+                throw new ProcessingException("Error processing ServiceType message. Message received but could not be processed.", e);
             }
         };
 
@@ -105,21 +113,26 @@ public class MessageQueueConsumer {
     @Bean
     Consumer<Message<LocationDTO>> receiveLocation() {
         return message -> {
-            log.info("Received Location: {}", message);
-            log.info("Payload: {}", message.getPayload());
-            switch (message.getPayload().getEventType()) {
-                case CREATE:
-                    locationRepository.save(new LocationResponse(
-                            message.getPayload().getLocation().id(),
-                            message.getPayload().getLocation().address(),
-                            message.getPayload().getLocation().latitude(),
-                            message.getPayload().getLocation().longitude(),
-                            message.getPayload().getLocation().country(),
-                            message.getPayload().getLocation().city(),
-                            message.getPayload().getLocation().municipality()
-                    ));
-                    log.info("Location saved: {}", message.getPayload().getLocation());
-                    break;
+            try{
+                log.info("Received Location: {}", message);
+                log.info("Payload: {}", message.getPayload());
+                switch (message.getPayload().getEventType()) {
+                    case CREATE:
+                        locationRepository.save(new LocationResponse(
+                                message.getPayload().getLocation().id(),
+                                message.getPayload().getLocation().address(),
+                                message.getPayload().getLocation().latitude(),
+                                message.getPayload().getLocation().longitude(),
+                                message.getPayload().getLocation().country(),
+                                message.getPayload().getLocation().city(),
+                                message.getPayload().getLocation().municipality()
+                        ));
+                        log.info("Location saved: {}", message.getPayload().getLocation());
+                        break;
+                }
+            } catch (Exception e) {
+                log.error("Error processing message: {}", e.getMessage());
+                throw new ProcessingException("Error processing Location message. Message received but could not be processed.", e);
             }
         };
     }
